f4291770410147a07d257b0606e0f873c0392590
AuroreM <aurore.malherbes@student.ecp.fr>
2015-11-23 17:25:16 +0100

    Réalisation fonction stockercollection

--- projetV1.py
+++ projetV1.py
@@ -1,21 +1,54 @@
-corpus = open("cacm.all", "r")
-corpus_contenu = corpus.read()
-#print(corpus_contenu)
-corpus_tokenized = corpus_contenu.split(" ")
-#print(corpus_tokenized)
+def stockercollection(path):
+		documents = {}
+		document ={}
+		corpus = open(path, "r")
+		separateurs = [".T",".W",".B",".A",".N",".X",".K"]
+		id = -1
 
-no_punctuation_tokens = []
-for token in corpus_tokenized:
-    token = token.replace(".","")
-    token = token.replace(",","")
-    token = token.replace("'", "")
-    token = token.replace(";", "")
-    token = token.replace("\n", "")
-    token = token.replace("\t", "")
-    no_punctuation_tokens.append(token)
+		for line in corpus:
+			if (line[0:2] == ".I"):
+				if (id <> -1):
+					documents[id]=document
+					document = {}
+				id = int(line[3:])
+			elif (line[0:2] in separateurs):
+				separateur = line[0:2]
+			elif (line[0:2] not in separateurs and id <> -1 and line[0:2] <> ".I"):
+				if (separateur in document.keys()):
+					if (separateur == ".A"):
+						document[separateur] = document[separateur] + [line.replace('\n', '')]
+					else:
+						document[separateur] = document[separateur] + line.replace('\n','')
+				else:
+					if (separateur == ".A"):
+						document[separateur] = [line.replace('\n', '')]
+					else:
+						document[separateur] = line.replace('\n','')
+		
+		documents[id] = document
+		return documents
+
+article = stockercollection("cacm.all")
+
+#print(article[1])
+
+def tokenized(path):
+
+	corpus = open(path, "r")
+	#print(corpus_tokenized)
+	corpus_contenu = corpus.read()
+
+	corpus_tokenized = corpus_contenu.split(" ")
+	no_punctuation_tokens = []
+	for token in corpus_tokenized:
+	    token = token.replace(".","")
+	    token = token.replace(",","")
+	    token = token.replace("'", "")
+	    token = token.replace(";", "")
+	    token = token.replace("\n", "")
+	    token = token.replace("\t", "")
+	    no_punctuation_tokens.append(token)
+	return no_punctuation_tokens
     
-print(no_punctuation_tokens)
-
-#ponctuation_to_remove={"1":",", "2":.}
-
+#print(no_punctuation_tokens)
 
